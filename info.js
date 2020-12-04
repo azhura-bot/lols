51a128241b7255a16124c0eaf9cb3b949543ec98
Azhura <75462828+azhura-bot@users.noreply.github.com>
2020-12-04 11:14:11 +0700

    Add files via upload

---
+++ info.js
@@ -0,0 +1,30 @@
+exports.info = (id, BotName, corohelp, tampilTanggal, tampilWaktu, instagramlu, whatsapplu, kapanbotaktif, grupch1, grupch2) => {
+	return `🔰 -----[ *MENU ${Azhura}* ]----- 🔰
+  
+Hi, *${id.split("@s.whatsapp.net")[0]}* 👋️
+Berikut adalah info pada bot ini!✨
+
+[ ️ *${tampilTanggal}* ]
+[️ *${tampilWaktu}* ]
+
+🔴 INFO BOT! :
+
+=> *AUTHOR*: ./Azhuras
+
+
+♻️ JANGAN  LUPA DONASI AGAR BOT AKTIF TERUS!
+♻️ MAU DONASI? SILAHKAN KETIK #donate
+
+📺 *Promosi* :
+
+✅ Follow akun instagram admin ${@fal.naf}
+
+⚠️ INFORMASI COVID-19 TERBARU!
+
+⚠️ POSITIF: *${corohelp.confirmed.value}*
+⚠️ SEMBUH: *${corohelp.recovered.value}*
+⚠️ MENINGGAL: *${corohelp.deaths.value}*
+⚠️ UPDATE: *${corohelp.lastUpdate}*
+
+♻️ _TETAP JAGA KESEHATAN DAN SELALU PAKAI MASKER!_
+
